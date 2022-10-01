9f4e3713a252491ce8b71dee08c4af57914864c1
dalia-alawni <daliaalawneh7@gmail.com>
2022-10-01 11:06:54 +0300

    preject added

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+// let img = document.getElementById('img');
+// document.body.onload = () =>{
+//     img.style.animationName=opacity;
+//     img.style.animationDuration=1;
+// }
+
+// $(window).scroll(function(){
+//     if($(this).scrollTop() >= $(".products .product-item").offset().top){
+//         $(".products .product-item").fadeIn(2000);
+//         $(".trending-products .trending-items").animate();
+//     }
+    
+// });
